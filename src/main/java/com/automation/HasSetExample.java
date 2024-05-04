510745c6352b21b7a3952a497afe1334086fff42
Ajit <ajitkarwal1980@gmail.com>
2024-05-04 15:39:03 +0530

    First Commit

---
+++ src/main/java/com/automation/HasSetExample.java
@@ -0,0 +1,11 @@
+package com.automation;
+
+import java.util.HashSet;
+
+public class HasSetExample {
+    public static void main(String[] args) {
+
+        HashSet<String> hashSet = new HashSet<>();
+        hashSet.add("Ajit");
+    }
+}
